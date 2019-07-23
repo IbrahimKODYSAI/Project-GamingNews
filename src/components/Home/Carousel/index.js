aafa3c68b70d8560ce6c40f6cf739e857106e20a
IbrahimKODYSAI <saneda.i@live.fr>
2019-07-23 19:48:53 +0200

    ajout des pages login signUp et les route [Login]

---
+++ src/components/Home/Carousel/index.js
@@ -0,0 +1,72 @@
+// == Import : npm
+import React, { Component } from 'react';
+import Coverflow from 'react-coverflow';
+import YouTube from 'react-youtube';
+
+// == Import : local
+import './carousel.scss';
+
+// == Composant
+class Carousel extends Component {
+  state ={}
+  render() {
+
+    const opts = {
+      height: '200',
+      width: '400',
+      playerVars: {
+        modestbranding: 1,
+        autohide: 1,
+        showinfo: 0
+      }
+    };
+
+  return (
+    
+    <div id="Carousel">
+
+      <Coverflow
+        className="media"
+        displayQuantityOfSide={2}
+        navigation={true}
+        enableScroll
+        clickable
+        active={0}
+        media={{
+        '@media (max-width: 900px)': {
+          width: '600px',
+          height: '150px'
+        },
+        '@media (min-width: 900px)': {
+          width: '100%',
+          height: '300px'
+        }
+      }}
+      >
+
+        <YouTube
+          videoId="TcZyiYOzsSw"
+          opts={opts}
+        />
+
+        <YouTube
+          videoId="qIcTM8WXFjk"
+          opts={opts}
+        />
+
+        <YouTube
+          videoId="ENjwexZnLPI"
+          opts={opts}
+        />
+
+      </Coverflow>
+
+    </div>
+
+  );
+  }
+}
+
+
+// == Export
+export default Carousel;
